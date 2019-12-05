6b8455fe351dabbacc7f480945d614997cc7ece3
marpyl <marpyl69@gmail.com>
2019-12-05 16:49:21 +0200

    ex square area

---
+++ SquareArea/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProgrammingBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int square = int.Parse(Console.ReadLine()); // 2
+            int total = square * square;
+            Console.WriteLine(total);
+        }
+    }
+}
