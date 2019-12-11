fcacecdff9fca34f1996b819cde6df7dc525ca1b
marpyl <marpyl69@gmail.com>
2019-12-11 10:49:51 +0200

    add ex personal titles

---
+++ NestedConditionalStatements/PersonalTitles/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double age = double.Parse(Console.ReadLine());
+            string name = Console.ReadLine();
+
+            if (name == "f")
+            {
+
+                if (age < 16)
+                {
+
+                    Console.WriteLine("Miss");
+                }
+
+                else if (age >= 16)
+                {
+
+                    Console.WriteLine("Ms.");
+                }
+            }
+
+            else if (name == "m")
+            {
+
+                if (age < 16)
+                {
+
+                    Console.WriteLine("Master");
+
+                }
+
+                else if (age >= 16)
+                {
+
+                    Console.WriteLine("Mr.");
+                }
+
+            }
+        }
+    }
+}
