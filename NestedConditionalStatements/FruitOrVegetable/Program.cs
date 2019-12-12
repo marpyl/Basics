8502e36489aba00d584c33b699d3e19afc18f193
marpyl <marpyl69@gmail.com>
2019-12-12 10:55:27 +0200

    fruit or vegetable ex

---
+++ NestedConditionalStatements/FruitOrVegetable/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+
+            if (product == "banana" || product == "apple" || product == "kiwi" || product == "cherry" || product == "lemon" || product == "grapes")
+            {
+
+                Console.WriteLine("fruit");
+            }
+
+            else if (product == "tomato" || product == "cucumber" || product == "pepper" || product == "carrot")
+
+            {
+
+                Console.WriteLine("vegetable");
+            }
+
+            else
+            {
+
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
