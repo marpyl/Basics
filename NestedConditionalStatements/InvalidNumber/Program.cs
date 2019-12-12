7f3eadf5872175cad7d8840459de5dfae5781184
marpyl <marpyl69@gmail.com>
2019-12-12 10:56:27 +0200

    invalid number ex

---
+++ NestedConditionalStatements/InvalidNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number = int.Parse(Console.ReadLine());
+            bool total = number >= 100 && number <= 200 || number == 0;
+
+
+            if (!total) { Console.WriteLine("invalid"); }
+        }
+    }
+}
