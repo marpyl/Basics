38259997606a3b6a3845e995335b8ed3e80baa62
marpyl <marpyl69@gmail.com>
2019-12-13 09:46:49 +0200

    cinema ex

---
+++ NestedConditionalStatements/Cinema/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+            double total = rows * cols;
+
+            if (type == "Premiere")
+            {
+                total *= 12;
+                
+            }
+            else if (type == "Normal")
+            {
+                total *= 7.50;
+            }
+            else if (type == "Discount") 
+            {
+                total *= 5;
+            }
+
+            Console.WriteLine($"{total:f2} leva");
+        }
+    }
+}
