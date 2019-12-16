2e469d786e770a89d25ef01912f36c9eb39d7904
marpyl <marpyl69@gmail.com>
2019-12-16 10:15:52 +0200

    divide without reminder ex

---
+++ ForLoop/Divide Without Reminder/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+            double p1 = 0;
+            double p2 = 0;
+            double p3 = 0;
+
+            for (int i = 0; i < num; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (number % 2 == 0)
+                {
+                    p1++;
+                }
+                if (number % 3 == 0)
+                {
+                    p2++;
+                }
+                if (number % 4 == 0)
+                {
+                    p3++;
+                }
+            }
+
+            Console.WriteLine($"{((p1 / num) * 100):f2}%");
+            Console.WriteLine($"{((p2 / num) * 100):f2}%");
+            Console.WriteLine($"{((p3 / num) * 100):f2}%");
+
+        }
+    }
+}
