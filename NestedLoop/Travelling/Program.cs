779bd137a9d429b0438689e0328f715f4cabeda5
marpyl <marpyl69@gmail.com>
2019-12-16 10:16:58 +0200

    travelling ex

---
+++ NestedLoop/Travelling/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+
+namespace exams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            while (true)
+            {
+                string destination = Console.ReadLine();
+
+
+                if (destination == "End")
+                {
+                    break;
+                }
+                double total = 0.0;
+                double budget = double.Parse(Console.ReadLine());
+
+                for (int i = 1; i <= budget; i++)
+                {
+
+                    double sum = double.Parse(Console.ReadLine());
+                    total += sum;
+
+                    if (total >= budget)
+                    {
+                        Console.WriteLine("Going to " + destination + "!");
+
+                        break;
+                    }
+
+                }
+
+            }
+
+
+            //int n = int.Parse(Console.ReadLine());
+            //int total = 0;
+
+            //for (int i = 0; i <= n; i++)
+            //{
+            //    for (int j = 0; j <= n; j++)
+            //    {
+            //        for (int k = 0; k <= n; k++)
+            //        {
+            //            for (int l = 0; l <= n; l++)
+            //            {
+            //                for (int m = 0; m <= n; m++)
+            //                {
+
+
+
+            //                    if (i+j+k+l+m == n)
+            //                    {
+            //                        total++;
+
+            //                    }
+
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
+            //Console.WriteLine(total);
+        }
+    }
+}
