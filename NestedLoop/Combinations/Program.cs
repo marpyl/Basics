c3ba2f6d18eb789c7c72450bae3db55ad0710a41
marpyl <marpyl69@gmail.com>
2019-12-17 09:01:09 +0200

    combinations ex

---
+++ NestedLoop/Combinations/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace exams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int total = 0;
+
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = 0; j <= n; j++)
+                {
+                    for (int k = 0; k <= n; k++)
+                    {
+                        for (int l = 0; l <= n; l++)
+                        {
+                            for (int m = 0; m <= n; m++)
+                            {
+
+
+
+                                if (i + j + k + l + m == n)
+                                {
+                                    total++;
+
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
