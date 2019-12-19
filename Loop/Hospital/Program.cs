05041d6cb31f56e6a7ea6aa36ba58b8e78523c31
marpyl <marpyl69@gmail.com>
2019-12-19 10:41:33 +0200

    hospital ex

---
+++ Loop/Hospital/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int doctors = 7;
+            int treatedPaciants = 0;
+            int untreatedPaciants = 0;
+
+            for (int i = 1; i <= days; i++)
+            {
+                int pacients = int.Parse(Console.ReadLine());
+
+
+                if ((i%3 == 0) && (untreatedPaciants>treatedPaciants))
+                {
+                    doctors++;
+                }
+
+                
+                if (pacients>= doctors)
+                {
+                    untreatedPaciants += (pacients - doctors);
+                    treatedPaciants += doctors;
+                }
+                else if (doctors>pacients)
+                {
+                    treatedPaciants += pacients;
+                }
+                
+            }
+
+            Console.WriteLine($"Treated patients: {treatedPaciants}.");
+            Console.WriteLine($"Untreated patients: {untreatedPaciants}.");
+
+
+
+        }
+    }
+}
