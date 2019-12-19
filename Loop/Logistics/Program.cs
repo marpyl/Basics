14d4179ae1998afd00ad9abfd03576929dddae35
marpyl <marpyl69@gmail.com>
2019-12-19 10:42:03 +0200

    logistics ex

---
+++ Loop/Logistics/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logistics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int loads = int.Parse(Console.ReadLine());
+            
+            int bus = 0;
+            int truck = 0;
+            int train = 0;
+            double totalTons = bus + truck + train;
+
+            for (int i = 0; i < loads; i++)
+            {
+                int tons = int.Parse(Console.ReadLine());
+
+                if (tons> 0 && tons <=3)
+                {
+                    
+                    bus += tons;
+                }
+                else if (tons > 3 && tons <= 11)
+                {
+                    
+                    truck += tons;
+                }
+                else if (tons > 11)
+                {
+                    
+                    train += tons;
+                }
+            }
+
+            
+
+            Console.WriteLine($"{((bus*200 + truck *175 + train * 120)/totalTons):f2}");
+            Console.WriteLine($"{(bus/totalTons*100):f2}%");
+            Console.WriteLine($"{(truck/totalTons*100):f2}%");
+            Console.WriteLine($"{(train/totalTons*100):f2}%");
+        }
+    }
+}
