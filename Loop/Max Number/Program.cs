30dd583ed711c80a8230bcc9cfae2dc939fee162
marpyl <marpyl69@gmail.com>
2020-01-06 10:27:54 +0200

    max number ex

---
+++ Loop/Max Number/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Max_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int minNumber = int.MaxValue;
+            int maxNumber = int.MinValue;
+
+            for (int i = 0; i < num; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (number > maxNumber)
+                {
+                    maxNumber = number;
+                    
+                }
+            }
+
+            Console.WriteLine(maxNumber);
+        }
+    }
+}
