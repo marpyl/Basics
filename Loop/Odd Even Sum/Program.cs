deec90a87a4e7a34377830784bc140c5725a5673
marpyl <marpyl69@gmail.com>
2020-01-07 09:25:20 +0200

    odd even sum ex

---
+++ Loop/Odd Even Sum/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odd_Even_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int leftSum = 0;
+            int rightSum = 0;
+
+            for (int i = 1; i <= num; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (i%2 == 0)
+                {
+                    leftSum += number;
+                }
+                else
+                {
+                    rightSum += number;
+                }
+
+            }
+            
+
+            if (leftSum == rightSum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = " + leftSum);
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine("Diff = " + Math.Abs(rightSum-leftSum));
+            }
+        }
+    }
+}
