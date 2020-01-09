56cbc9808d459f79a4f3ce4e9d977f0e84560eb2
marpyl <marpyl69@gmail.com>
2020-01-09 16:16:59 +0200

    sum numbers ex

---
+++ Loop/Sum numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 0; i < num; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+               
+                sum += number;
+                
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
