37b7bfe76bd18be5d26a666c01737e96af6bf67f
marpyl <marpyl69@gmail.com>
2020-02-19 16:56:51 +0200

    fire work test ex

---
+++ Exams/11032019/FireWorkTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FireWorkTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> text = Console.ReadLine().Split('#', StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            for (int i = 0; i < text.Count; i++)
+            {
+                Console.WriteLine(text[4]);
+            }
+
+
+            Console.WriteLine(string.Join(" ", text));
+
+            foreach (var item in text)
+            {
+                Console.WriteLine(item);
+            }
+        }    
+
+    }
+}
