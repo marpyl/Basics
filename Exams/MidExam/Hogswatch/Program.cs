58b2e234fe1134fe3d7939db0036062b63770a67
marpyl <marpyl69@gmail.com>
2020-02-26 10:11:52 +0200

    hogswatch ex

---
+++ Exams/MidExam/Hogswatch/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Hogswatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ne e zavarshena
+            int homes = int.Parse(Console.ReadLine());
+
+            int totalPresents = int.Parse(Console.ReadLine());
+            int count = 0;
+            int current = 0;
+            int total = 0;
+
+            for (int i = 0; i < homes; i++)
+            {
+                int childrens = int.Parse(Console.ReadLine());
+
+                totalPresents -= childrens;
+
+                if (totalPresents >= 0 && i == homes-1)
+                {
+                    Console.WriteLine(totalPresents);
+                    break;
+                }
+
+                else if (totalPresents < 0 && i !=homes - 1)
+                {
+                    count++;
+                    totalPresents += childrens;
+                    current = Math.Abs(totalPresents - childrens);
+                    totalPresents += totalPresents / i * (homes - i);
+                    total = totalPresents + current;
+                    Console.WriteLine("Current " + current);
+                    Console.WriteLine("Total " + total);
+                }
+                
+                
+            }
+
+            if (count > 0)
+            {
+                Console.WriteLine(count);
+                Console.WriteLine(total);
+            }
+        }
+    }
+}
