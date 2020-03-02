318165eec9248bd2b75af08637cb6d9f00b2a691
marpyl <marpyl69@gmail.com>
2020-03-02 17:26:27 +0200

    mid exam preparation

---
+++ Exams/MidExam/MidExamPreparation/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace GoogleSearches
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalDays = int.Parse(Console.ReadLine());
+            int users = int.Parse(Console.ReadLine());
+            double money = double.Parse(Console.ReadLine());
+            double total = 0;
+
+            for (int i = 0; i < users; i++)
+            {
+                int words = int.Parse(Console.ReadLine());
+
+                if (i % 3 == 0)
+                {
+                    total += money * 3;
+                }
+
+                if (words > 5)
+                {
+                    continue;
+                }
+                
+
+                else if (words == 1)
+                {
+                    total += money * 2;
+                }
+
+                else
+                {
+                    total += money;
+                }
+
+                
+
+                
+
+            }
+
+            double moneyTotal = total * totalDays;
+
+            Console.WriteLine($"Total money earned for {totalDays} days: {moneyTotal:f2}");
+
+        }
+    }
+}
