175651305975bfac58f803f163b7ce9af827170b
marpyl <marpyl69@gmail.com>
2020-03-03 17:38:42 +0200

    reception ex

---
+++ Exams/MidExam/SoftuniReception/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SoftuniReception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+
+            int studentsPerHour = first + second + third;
+            int all = students / studentsPerHour;
+            int hours = 0;
+
+            while (students > 0)
+            {
+                hours++;
+                students -= studentsPerHour;
+                if (hours % 4 == 0)
+                {
+                    hours++;
+                }
+            }
+           
+
+            Console.WriteLine($"Time needed: {hours}h.");
+        }
+    }
+}
