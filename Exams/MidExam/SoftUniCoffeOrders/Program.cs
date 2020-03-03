cc51e046469df9edfd773b11a7b3e2ad4a7ccf8b
marpyl <marpyl69@gmail.com>
2020-03-03 17:38:16 +0200

    coffee orders ex

---
+++ Exams/MidExam/SoftUniCoffeOrders/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace SoftUniCoffeOrders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            decimal total = 0;
+
+            for (int i = 0; i < number; i++)
+            {
+                decimal price = decimal.Parse(Console.ReadLine());
+                DateTime date = DateTime.ParseExact(Console.ReadLine(), "d/M/yyyy", CultureInfo.InvariantCulture);
+                long capsules = long.Parse(Console.ReadLine());
+
+                decimal currentPrice = DateTime.DaysInMonth(date.Year, date.Month) * capsules * price;
+                total += currentPrice;
+                Console.WriteLine($"The price for the coffee is: ${currentPrice:f2}");
+
+
+            }
+
+            Console.WriteLine($"Total: ${total:f2}");
+        }
+    }
+}
