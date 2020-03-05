016ac593cc342d87c258301969168e96731fbaf5
marpyl <marpyl69@gmail.com>
2020-03-05 17:08:20 +0200

    cooking factory ex

---
+++ Exams/MidExam/CookingFactoryEx3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CookingFactoryEx3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            List<int> newList = new List<int>();
+            int sum = int.MinValue;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "Bake It!")
+                {
+                    break;
+                }
+
+                numbers = command.Split('#').Select(int.Parse).ToList();
+
+                int currentSum = numbers.Sum();
+                
+                int minTotal = int.MinValue;
+
+                int currentCount = numbers.Count();
+                double total = (double)currentSum / currentCount;
+
+                
+
+                if (currentSum > sum && minTotal < total)
+                {
+                    sum = currentSum;
+                    
+                    minTotal = (int)total;
+                    newList = numbers;
+
+
+                }
+                
+                
+            }
+            Console.WriteLine($"Best Batch quality: {sum}");
+            Console.WriteLine(string.Join(" ", newList));
+            
+        }
+    }
+}
