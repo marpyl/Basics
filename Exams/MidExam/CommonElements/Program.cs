7c84f34ad097146ced39bcc5d61e0a122d5edf3f
marpyl <marpyl69@gmail.com>
2020-03-07 17:32:31 +0200

    common elements ex

---
+++ Exams/MidExam/CommonElements/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CommonElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 80 tochki
+
+            List<string> firstList = Console.ReadLine().Split().ToList(); 
+            List<string> secondList = Console.ReadLine().Split().ToList();
+            List<string> newLine = new List<string>();
+
+            int count = 0;
+
+            if (firstList.Count >= secondList.Count)
+            {
+                count = firstList.Count;
+            }
+            else
+            {
+                count = secondList.Count;
+            } 
+
+            for (int i = 0; i < count; i++)
+            {
+                if (firstList.Contains(secondList[i]))
+                {
+                    newLine.Add(secondList[i]);
+                }
+            }
+
+            
+            
+
+         
+
+            Console.WriteLine(string.Join(" ", newLine));
+        }
+    }
+}
