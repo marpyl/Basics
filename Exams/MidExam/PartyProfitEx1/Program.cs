c8e61d6e1b9977216de43c1b91d399449461cd79
marpyl <marpyl69@gmail.com>
2020-03-10 09:58:59 +0200

    party profit ex

---
+++ Exams/MidExam/PartyProfitEx1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace PartyProfitEx1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+
+            double coins = 0.00;
+
+            for (int i = 1; i <= days; i++)
+            {
+
+                
+
+                if (i % 10 == 0)
+                {
+
+                    people -= 2;
+                }
+                if (i % 15 == 0)
+                {
+
+                    people += 5;
+                }
+
+                coins += 50;
+                coins -= 2 * people;
+
+                if (i % 3 == 0)
+                {
+
+                    coins -= 3 * people;
+                }
+                if (i % 5 == 0)
+                {
+
+                    coins += 20 * people;
+                    if (i % 3 == 0)
+                    {
+
+                        coins -= 2 * people;
+                    }
+                }
+
+
+            }
+
+            double total = Math.Floor(coins / people);
+
+            Console.WriteLine(people + " companions received " + total + " coins each.");
+        }
+    }
+}
