85157555e620b736eddaed305c3e2f9025ebbcb3
marpyl <marpyl69@gmail.com>
2020-03-11 09:08:36 +0200

    preparation

---
+++ Exams/MidExam/PreparationMidExamTF/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            List<int> people = new List<int>();
+            int sum = 0;
+            
+
+            for (int i = 0; i < number; i++)
+            {
+                int persons = int.Parse(Console.ReadLine());
+                people.Add(persons);
+                sum += persons;
+            }
+
+            Console.WriteLine(string.Join(" ", people));
+
+            Console.WriteLine(sum);
+        }
+    }
+}
