052928cbd954edf2c6a81ceda547452de7d73464
marpyl <marpyl69@gmail.com>
2020-03-11 09:08:10 +0200

    password validator ex

---
+++ Exams/MidExam/PasswordValidator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PasswordValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sum();
+
+        }
+
+        private static void Sum()
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+
+            int sum = first + second;
+            int result = sum - third;
+            Console.WriteLine(result);
+        }
+    }
+}
